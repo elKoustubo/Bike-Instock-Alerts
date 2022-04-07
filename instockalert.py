5eb6a96ef7b6886af481205c6b19766660376c40
K <koustubj30@gmail.com>
2022-04-07 09:42:52 -0400

    Added comments

--- instockalert.py
+++ instockalert.py
@@ -1,9 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
-
+# Function to get url of our desired bike and in its desired size
+# Please note that url must point to a specific bike and size for our getalert function to work correctly
 def geturl(opbike:int=1, optype:int=1, opsize:str='S'): # My bike size is S, hence it's default
-    if opbike == 1: #Endurace family
+    if opbike == 1: #Endurace bike family
         mainurl = "https://www.canyon.com/en-us/road-bikes/endurance-bikes/endurace/"
         if optype == 1: # endurace-cf-sl-7-disc
             opt = "cf-sl/endurace-cf-sl-7-disc/2962.html?dwvar_2962_pv_rahmenfarbe=YE%2FBK&dwvar_2962_pv_rahmengroesse=" + opsize
@@ -11,7 +12,7 @@ def geturl(opbike:int=1, optype:int=1, opsize:str='S'): # My bike size is S, hen
             opt = "al/endurace-8-disc/2854.html?dwvar_2854_pv_rahmenfarbe=BK%2FBU&dwvar_2854_pv_rahmengroesse=" + opsize
         elif optype == 3: # BLACK endurace-8-disc
             opt = "al/endurace-8-disc/2854.html?dwvar_2854_pv_rahmenfarbe=BK%2FBK&dwvar_2854_pv_rahmengroesse=" + opsize
-    elif opbike == 2: # 
+    elif opbike == 2: # Grizl bike family
         mainurl = "https://www.canyon.com/en-us/gravel-bikes/bike-packing/grizl/"
         if optype == 1: # grizl-cf-sl-6
             opt = "cf-sl/grizl-cf-sl-6/3243.html?dwvar_3243_pv_rahmenfarbe=YE&dwvar_3243_pv_rahmengroesse=" + opsize
@@ -23,32 +24,38 @@ def geturl(opbike:int=1, optype:int=1, opsize:str='S'): # My bike size is S, hen
 
     return mainurl+opt
 
-
+# Alert if the item is available to be added to cart i.e. it is in stock, yay!
 def getalert(url = geturl()):
+    alert = False # Default 
     bike = url.split('/')[8] # Bike name
     try:
         page = requests.get(url)
     except:
-        page.status_code = 0
+        print("Failed to connect!!!")
         
-
+    # Status Code = 200 indicates "everything went as expected"
     if page.status_code == 200:
-        soup = BeautifulSoup(page.content, 'html.parser')
+        soup = BeautifulSoup(page.content, 'html.parser') # Parse page contents
+        # If the item is in stock then "Add to cart" button is available. Using BS4 to find desired "button" class
         to_cart = soup.findAll("button", {"class":"button js-addToCart-button productDescription__addToCart js-chooseVariation button--primary"})
-        alert = (len(to_cart) != 0)
+        alert = (len(to_cart) != 0) # True if "Add to cart" button exists
         return alert, bike
 
+# Using Twilio free account to send myself SMS if the desired item is in stock!
 def checkstock(opbike:int=1, optype:int=1, opsize:str='S'):
     alert, bike = getalert(geturl(opbike,optype,opsize))
 
     if alert:
-        import killi
+        import killi # Twilio account token info
         from twilio.rest import Client
+
+        # Initiate Twilio client
         def setup_twilio_client():
             account_sid = killi.TWILIO_ACCOUNT_SID()
             auth_token = killi.TWILIO_AUTH_TOKEN()
             return Client(account_sid, auth_token)
 
+        # Define SMS content, sender, receiver  
         def send_notification():
             twilio_client = setup_twilio_client()
             twilio_client.messages.create(
@@ -66,5 +73,5 @@ checkstock(optype=3)
 checkstock(opbike=2, optype=1)
 checkstock(opbike=2, optype=2)
 
-# checkstock(opbike=3)  # Uncomment for SMS Alert Test
+#checkstock(opbike=3)  # Uncomment for SMS Alert Test 
 
