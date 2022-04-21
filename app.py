753ed65cd8a177072f3f694532361afc9d489487
K <koustubj30@gmail.com>
2022-04-20 23:38:10 -0400

    Updated Streamlit app

--- app.py
+++ app.py
@@ -1,36 +1,33 @@
 from distutils.fancy_getopt import OptionDummy
 import streamlit as st
 
-from instockalert import geturl, getalert, checkstock
+from instockalert import geturl, getalert
 
 st.set_page_config(page_icon='📦', page_title='Is my bike in stock?')
 
-
-
-bike = st.radio("Which bike's stock do you wish to check?", ('Endurace','Grizl','Test'))
-opbike = ['Endurace','Grizl','Test'].index(bike) + 1 
-
-if opbike == 1:
-    type = st.radio("Name of the bike", ('Endurace-cf-sl-7-disc','BLUE endurace-8-disc','BLACK endurace-8-disc'))
-    optype = ['Endurace-cf-sl-7-disc','BLUE endurace-8-disc','BLACK endurace-8-disc'].index(type) + 1
-elif opbike == 2:
-    type = st.radio("Name of the bike", ('grizl-cf-sl-6', 'grizl-7'))
-    optype = ['grizl-cf-sl-6', 'grizl-7'].index(type) + 1
-else:
-    st.write('Please select XL or 2XL size for testing')
-    optype = 42
+with open('bike_urls.txt') as bikes:
+    bikes = bikes.readlines()
+models = []
+colors = []
+for url in bikes:
+    models.append(url.split('/')[8])
+    colors.append(url.split('=')[1].replace('%2','-').strip('\n')) # Cleaning up color substring
+# Our first option is for testing our code on known model that is in stock
+models[0] = 'TEST  ' + models[0]
+bike = st.radio("Which bike's stock do you wish to check?", tuple(models))
+opbike = models.index(bike)  
 
 
 size = st.radio("Size", ('S','M','L','XL','2XL'))
 
 try:
-    alert, name = getalert(geturl(opbike,optype,size))
+    alert, name = getalert(geturl(opbike,size))
 except:
     st.markdown(f" #### Please make valid selection ")
     alert = False
 
 if alert:
     st.markdown(f" #### Bike IS in stock! \
-                   \nfollow the link to buy {geturl(opbike,optype,size)}")
+                   \nfollow the link to buy {geturl(opbike,size)}")
 else:
     st.markdown(f" #### Bike IS NOT in stock! ☹️ ")
